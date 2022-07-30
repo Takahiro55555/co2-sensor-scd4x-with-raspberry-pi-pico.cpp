bc03102806ca1f0fbbf561e031e59105bd042c78
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-07-31 08:00:09 +0900

    add: SCD4x class (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include "scd4x.hpp"
+
 int main() {
-    return 0;
+  /* Pico settings */
+  const int SDA_PIN = 26; /* GP26 = Pin.31 = I2C1 SDA */
+  const int SCL_PIN = 27; /* GP27 = Pin.32 = I2C1 SCL */
+  i2c_inst_t *i2c_num = i2c1;
+
+  stdio_init_all();
+  i2c_init(i2c_num, 100000);                    // use i2c1 module, SCL = 100kHz
+  gpio_set_function(SDA_PIN, GPIO_FUNC_I2C); // set function of SDA_PIN I2C
+  gpio_set_function(SCL_PIN, GPIO_FUNC_I2C); // set function of SCL_PIN I2C
+  gpio_set_pulls(SDA_PIN, true, false); // enable internal pull-up of SDA_PIN
+  gpio_set_pulls(SCL_PIN, true, false); // enable internal pull-up of SCL_PIN
+
+  SCD4x scd4x = SCD4x(i2c_num);
+
+  return 0;
 }
---
+++ src/scd4x.hpp
@@ -0,0 +1,46 @@
+#ifndef SCD4X_HPP
+#define SCD4X_HPP
+
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+/* SCD4x default i2c address */
+const uint16_t SCD4X_DEFAULT_ADDR = 0x62;
+
+class SCD4x {
+public:
+  SCD4x(i2c_inst_t *_i2c_num);
+  SCD4x(i2c_inst_t *_i2c_num, uint16_t _i2c_address);
+  uint8_t generate_crc(const uint8_t *data, uint16_t count);
+
+private:
+  /* SCD4x user settings */
+  const i2c_inst_t *i2c_num;
+  const uint16_t i2c_address;
+
+  /* SCD4x commands */
+  const uint16_t SCD4X_REINIT = 0x3646;
+  const uint16_t SCD4X_FACTORYRESET = 0x3632;
+  const uint16_t SCD4X_FORCEDRECAL = 0x362F;
+  const uint16_t SCD4X_SELFTEST = 0x3639;
+  const uint16_t SCD4X_DATAREADY = 0xE4B8;
+  const uint16_t SCD4X_STOPPERIODICMEASUREMENT = 0x3F86;
+  const uint16_t SCD4X_STARTPERIODICMEASUREMENT = 0x21B1;
+  const uint16_t SCD4X_STARTLOWPOWERPERIODICMEASUREMENT = 0x21AC;
+  const uint16_t SCD4X_READMEASUREMENT = 0xEC05;
+  const uint16_t SCD4X_SERIALNUMBER = 0x3682;
+  const uint16_t SCD4X_GETTEMPOFFSET = 0x2318;
+  const uint16_t SCD4X_SETTEMPOFFSET = 0x241D;
+  const uint16_t SCD4X_GETALTITUDE = 0x2322;
+  const uint16_t SCD4X_SETALTITUDE = 0x2427;
+  const uint16_t SCD4X_SETPRESSURE = 0xE000;
+  const uint16_t SCD4X_PERSISTSETTINGS = 0x3615;
+  const uint16_t SCD4X_GETASCE = 0x2313;
+  const uint16_t SCD4X_SETASCE = 0x2416;
+
+  /* SCD4x checksum */
+  const uint8_t CRC8_POLYNOMIAL = 0x31;
+  const uint8_t CRC8_INIT = 0xFF;
+};
+#endif
