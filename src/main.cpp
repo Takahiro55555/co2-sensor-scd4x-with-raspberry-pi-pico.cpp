d2c59d4446ef99a9630ab78b54cffcf97338b8ab
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-08-04 23:05:53 +0900

    update: ポーリングの間隔を短くした

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main()
   const int SCL_PIN = 27; /* GP27 = Pin.32 = I2C1 SCL */
   i2c_inst_t* i2c_num = i2c1;
 
+  /* i2c setup */
   stdio_init_all();
   i2c_init(i2c_num, 100000);                  // use i2c1 module, SCL = 100kHz
   gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);  // set function of SDA_PIN I2C
@@ -14,14 +15,14 @@ int main()
   gpio_set_pulls(SDA_PIN, true, false);       // enable internal pull-up of SDA_PIN
   gpio_set_pulls(SCL_PIN, true, false);       // enable internal pull-up of SCL_PIN
 
-  // sleep_ms(2000);
+  /* Read data */
   SCD4x scd4x = SCD4x(i2c_num);
   while(1) {
     if(scd4x.update()) {
       printf("%d [ppm], %f [C], %f [%%]\n", scd4x.co2(), scd4x.temperature(), scd4x.relative_humidity());
     }
-    sleep_ms(1000);
+    sleep_ms(100);
   }
 
-  return 0;
+  return 1;
 }
